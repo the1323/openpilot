8c7e2ef7425a8b5c26bba0fb9a9b8cfe7ed0c8dc
Miles Calloway <26465212+mcallbosco@users.noreply.github.com>
2022-02-22 11:49:42 -0500

    Disengage on gas (#10)
    * Disengage on gas toggle
    
    * Disengage on gas toggle
    
    * Fix missing params lib
    
    * Update interfaces.py
    
    Co-authored-by: Willem Melching <willem.melching@gmail.com>
    
    * Add param to process replay
    
    * Resolve conflicts
    
    * Resolve conflicts
    
    * Resolve conflicts
    
    * Add param to unsafeMode
    
    * Add param to unsafeMode
    
    * Add param to process replay
    
    Co-authored-by: Jason Wen <haibin.wen3@gmail.com>
    Co-authored-by: Jason Wen <47793918+sunnyhaibin@users.noreply.github.com>
    Co-authored-by: Willem Melching <willem.melching@gmail.com>

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -11,6 +11,7 @@ from selfdrive.config import Conversions as CV
 from selfdrive.controls.lib.drive_helpers import V_CRUISE_MAX
 from selfdrive.controls.lib.events import Events
 from selfdrive.controls.lib.vehicle_model import VehicleModel
+from common.params import Params
 
 GearShifter = car.CarState.GearShifter
 EventName = car.CarEvent.EventName
@@ -32,6 +33,7 @@ class CarInterfaceBase(ABC):
     self.steering_unpressed = 0
     self.low_speed_alert = False
     self.silent_steer_warning = True
+    self.disengage_on_gas = Params().get_bool("DisengageOnGas")
 
     if CarState is not None:
       self.CS = CarState(CP)
@@ -72,7 +74,7 @@ class CarInterfaceBase(ABC):
   def get_std_params(candidate, fingerprint):
     ret = car.CarParams.new_message()
     ret.carFingerprint = candidate
-    ret.unsafeMode = 0  # see panda/board/safety_declarations.h for allowed values
+    ret.unsafeMode = 1 if Params().get_bool("DisengageOnGas") else 0  # see panda/board/safety_declarations.h for allowed values
 
     # standard ALC params
     ret.steerControlType = car.CarParams.SteerControlType.torque
@@ -127,7 +129,7 @@ class CarInterfaceBase(ABC):
       events.add(EventName.wrongCarMode)
     if cs_out.espDisabled:
       events.add(EventName.espDisabled)
-    if cs_out.gasPressed:
+    if cs_out.gasPressed and self.disengage_on_gas:
       events.add(EventName.gasPressed)
     if cs_out.stockFcw:
       events.add(EventName.stockFcw)
@@ -156,7 +158,7 @@ class CarInterfaceBase(ABC):
       events.add(EventName.steerUnavailable)
 
     # Disable on rising edge of gas or brake. Also disable on brake when speed > 0.
-    if (cs_out.gasPressed and not self.CS.out.gasPressed) or \
+    if (self.disengage_on_gas and cs_out.gasPressed and not self.CS.out.gasPressed) or \
        (cs_out.brakePressed and (not self.CS.out.brakePressed or not cs_out.standstill)):
       events.add(EventName.pedalPressed)
 
