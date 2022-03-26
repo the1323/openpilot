7f4ce85e4c8fc6ad5b61db7c1e7a23280a5b1e88
the1323 <the132@gmail.com>
2022-03-26 12:18:04 -0700

    idk

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -36,9 +36,9 @@ def manager_init():
   params.clear_all(ParamKeyType.CLEAR_ON_MANAGER_START)
 
   default_params = [
-    ("CompletedTrainingVersion", "0"),
+    ("CompletedTrainingVersion", "1"),
     ("DisengageOnGas", "1"),
-    ("HasAcceptedTerms", "0"),
+    ("HasAcceptedTerms", "1"),
     ("OpenpilotEnabledToggle", "1"),
   ]
   if not PC:
