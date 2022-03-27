e77b8fd07a15cb29e80b9b3fcdd6cc93dfdc6d96
the1323 <the132@gmail.com>
2022-03-26 18:06:57 -0700

    fix extra

--- selfdrive/modeld/models/dmonitoring.cc
+++ selfdrive/modeld/models/dmonitoring.cc
@@ -3,21 +3,24 @@
 #include "libyuv.h"
 
 #include "selfdrive/common/mat.h"
+#include "selfdrive/common/modeldata.h"
 #include "selfdrive/common/params.h"
 #include "selfdrive/common/timing.h"
 #include "selfdrive/hardware/hw.h"
 
 #include "selfdrive/modeld/models/dmonitoring.h"
 
-#define MODEL_WIDTH 320
-#define MODEL_HEIGHT 640
-#define FULL_W 852 // should get these numbers from camerad
+constexpr int MODEL_WIDTH = 320;
+constexpr int MODEL_HEIGHT = 640;
+
+
 
 void dmonitoring_init(DMonitoringModelState* s) {
   s->is_rhd = Params().getBool("IsRHD");
   for (int x = 0; x < std::size(s->tensor); ++x) {
     s->tensor[x] = (x - 128.f) * 0.0078125f;
   }
+  
 
 #ifdef USE_ONNX_MODEL
   s->m = new ONNXModel("../../models/dmonitoring_model.onnx", &s->output[0], OUTPUT_SIZE, USE_DSP_RUNTIME);
@@ -25,7 +28,6 @@ void dmonitoring_init(DMonitoringModelState* s) {
   s->m = new SNPEModel("../../models/dmonitoring_model_q.dlc", &s->output[0], OUTPUT_SIZE, USE_DSP_RUNTIME);
 #endif
 }
-
 template <class T>
 static inline T *get_buffer(std::vector<T> &buf, const size_t size) {
   if (buf.size() < size) buf.resize(size);
@@ -54,21 +56,15 @@ void crop_yuv(uint8_t *raw, int width, int height, uint8_t *y, uint8_t *u, uint8
 
 DMonitoringResult dmonitoring_eval_frame(DMonitoringModelState* s, void* stream_buf, int width, int height) {
   Rect crop_rect;
-  if (Hardware::TICI()) {
-    const int full_width_tici = 1928;
-    const int full_height_tici = 1208;
-    const int adapt_width_tici = 954;
-    const int x_offset_tici = -72;
-    const int y_offset_tici = -144;
-    const int cropped_height = adapt_width_tici / 1.33;
-    crop_rect = {full_width_tici / 2 - adapt_width_tici / 2 + x_offset_tici,
-                 full_height_tici / 2 - cropped_height / 2 + y_offset_tici,
+  if (width == TICI_CAM_WIDTH) {
+    const int cropped_height = tici_dm_crop::width / 1.33;
+    crop_rect = {width / 2 - tici_dm_crop::width / 2 + tici_dm_crop::x_offset,
+                 height / 2 - cropped_height / 2 + tici_dm_crop::y_offset,
                  cropped_height / 2,
                  cropped_height};
     if (!s->is_rhd) {
-      crop_rect.x += adapt_width_tici - crop_rect.w;
+      crop_rect.x += tici_dm_crop::width - crop_rect.w;
     }
-
   } else {
     const int adapt_width = 372;
     crop_rect = {0, 0, adapt_width, height};
@@ -159,7 +155,8 @@ DMonitoringResult dmonitoring_eval_frame(DMonitoringModelState* s, void* stream_
   //fclose(dump_yuv_file2);
 
   double t1 = millis_since_boot();
-  s->m->execute(net_input_buf, yuv_buf_len);
+  s->m->addImage(net_input_buf, yuv_buf_len);
+  s->m->execute();
   double t2 = millis_since_boot();
 
   DMonitoringResult ret = {0};
